9255c17012df11e6c839a6782699dc3a5fc5ffcd
Abhishek Kumar <ak207326@gmail.com>
2023-01-19 13:27:46 +0530

    SOL

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> l = new ArrayList<>();
+        //Interface extends interface not implements
+        Set<Integer> set = new TreeSet<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        // map is not part of collection framework as this works on more than one datatype
+
+        //Collection is interface collections is a class/library
+        //not related to each other and are separate things
+        //Collections' functions help us to operate on Collection
+        //Remember Collection is a framework
+        //Wrapper classes are made in java because data type requires is generic classes and then only
+        // we can use integer and other data types.
+    }
+}
